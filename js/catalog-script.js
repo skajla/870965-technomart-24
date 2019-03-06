835324d355dced1ebebb584bc4f9d99f210cd1c2
Vika <vika@MacBook-Pro-Vika.local>
2019-03-06 14:13:48 +0300

    added js

---
+++ js/catalog-script.js
@@ -0,0 +1,25 @@
+var cartItems = document.querySelectorAll(".cart-link");
+var cartPopup = document.querySelector(".modal-cart");
+var cartClose = cartPopup.querySelector(".modal-close");
+
+cartItems.forEach(function(item) {
+  item.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    cartPopup.classList.add("modal-show");
+  });
+});
+
+cartClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  cartPopup.classList.remove("modal-show");
+});
+
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (cartPopup.classList.contains("modal-show")) {
+      cartPopup.classList.remove("modal-show");
+    }
+  }
+});
---
+++ js/script.js
@@ -0,0 +1,112 @@
+var link = document.querySelector(".contacts-btn");
+var popup = document.querySelector(".modal-write-us");
+var close = popup.querySelector(".modal-close");
+
+var login = popup.querySelector("[name=name-surname]");
+var form = popup.querySelector(".write");
+var email = popup.querySelector("[name=e-mail]");
+var message = popup.querySelector(".your-text");
+
+var isStorageSupport = true;
+  var storage = "";
+  
+  try {
+    storage = localStorage.getItem("login");
+  } catch (err) {
+    isStorageSupport = false;
+  };
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  if (storage) {
+    login.value = storage;
+    email.focus();
+  } else {
+    login.focus();
+  }
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
+
+
+form.addEventListener("submit", function (evt) {
+  if (!login.value || !email.value || !message.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  }  else {
+      if (isStorageSupport) {
+        localStorage.setItem("login", login.value);
+      }
+    }  
+});
+
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+      evt.preventDefault();
+    }
+  }
+});
+
+
+var mapLink = document.querySelector(".button-map");
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = mapPopup.querySelector(".modal-close");
+
+mapLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  
+  if (evt.keyCode === 27) {
+    if (mapPopup.classList.contains("modal-show")) {
+      mapPopup.classList.remove("modal-show");
+      evt.preventDefault();
+    }
+  }
+});
+
+
+
+var cartItems = document.querySelectorAll(".cart-link");
+var cartPopup = document.querySelector(".modal-cart");
+var cartClose = cartPopup.querySelector(".modal-close");
+
+cartItems.forEach(function(item) {
+  item.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    cartPopup.classList.add("modal-show");
+  });
+});
+
+cartClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  cartPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (cartPopup.classList.contains("modal-show")) {
+      cartPopup.classList.remove("modal-show");
+      evt.preventDefault();
+    }
+  }
+});
+
